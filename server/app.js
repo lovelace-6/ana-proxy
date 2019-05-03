c3dc25b825428b8b1c3556f3e5c026184c56f3eb
anacollado <34594754+anacollado@users.noreply.github.com>
2019-05-03 13:06:16 -0500

    changed authors and reviews proxy urls

--- server/app.js
+++ server/app.js
@@ -26,19 +26,19 @@ app.use(
 // hannah-service
 app.use(
   '/books/:id/reviews',
-  proxy({ target: 'http://localhost:3003', changeOrigin: true }),
+  proxy({ target: 'http://ec2-18-224-40-87.us-east-2.compute.amazonaws.com:3003', changeOrigin: true }),
 );
 
 // kaz-service
-app.use(
-  '/books/:id/info',
-  proxy({ target: 'http://localhost:3002', changeOrigin: true }),
-);
+// app.use(
+//   '/books/:id/info',
+//   proxy({ target: 'http://localhost:3002', changeOrigin: true }),
+// );
 
 // ginger-service
 app.use(
   '/books/:id/authors',
-  proxy({ target: 'http://localhost:3000', changeOrigin: true }),
+  proxy({ target: 'http://ec2-13-59-83-138.us-east-2.compute.amazonaws.com:9000', changeOrigin: true }),
 );
 
 module.exports = app;
